e04d87be8fc5eebd70988916ad89a50342a85667
Levike34 <levi.meadows@k3-labs.com>
2025-03-13 11:28:35 +0900

    fix typo

--- contracts/increment/src/lib.rs
+++ contracts/increment/src/lib.rs
@@ -112,7 +112,7 @@ impl IncrementContract {
 
         count += num;
 
-        env.storage().persistent().set(&COUNTER, &count);
+        env.storage().persistent().set(&COUNTER_64, &count);
         env.events().publish((symbol_short!("inc_u64"), num), count);
 
         count
--- contracts/increment/src/test.rs
+++ contracts/increment/src/test.rs
@@ -13,6 +13,8 @@ fn test() {
     let env = Env::default();
     let contract_id = env.register_contract(None, IncrementContract);
     let client = IncrementContractClient::new(&env, &contract_id);
+    assert_eq!(client.increment_u64(), 1);
+    assert_eq!(client.increment_by_u64(&33), 34);
 
     assert_eq!(client.increment(), 1);
     assert_eq!(client.increment(), 2);
@@ -27,8 +29,7 @@ fn test() {
     assert_eq!(client.set_value(&69), 69);
     assert_eq!(client.get_current_value(), 69);
 
-    assert_eq!(client.increment_u64(), 1);
-    assert_eq!(client.get_current_value_u64(), 1);
+    assert_eq!(client.get_current_value_u64(), 34);
     let events = client.env.events().all();
     for e in events {
         std::dbg!(e);
